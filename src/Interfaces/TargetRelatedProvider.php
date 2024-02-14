911e9b0083495a43beec8bfa095e1d8279f424af
Martin Procházka <juniwalk@outlook.cz>
2024-02-14 11:37:57 +0100

    Add interface to provide related targets

---
+++ src/Interfaces/TargetRelatedProvider.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/**43
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Nestor\Interfaces;
+
+interface TargetRelatedProvider
+{
+	public function getRecordRelated(): array;
+}
